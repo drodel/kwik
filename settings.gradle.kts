c23e553d0c71b7430c3181260d1474d5e5213afd
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2019-12-21 16:47:55 +0100

    Add fuzzer module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,10 @@ include("generator-test")
 
 include("evaluator")
 
+include("fuzzer-api")
+
 project(":generator-api").projectDir = file("generator/api")
 project(":generator-stdlib").projectDir = file("generator/stdlib")
 project(":generator-test").projectDir = file("generator/test")
+
+project(":fuzzer-api").projectDir = file("fuzzer/api")
